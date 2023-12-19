89172f359937ea98ab7010f9f06c414e914505a2
nanamargaretty <29603881-nanamargaretty@users.noreply.replit.com>
2023-12-19 16:47:16 +0000

    Initial commit

---
+++ css/js/scrip.js
@@ -0,0 +1,12 @@
+document.getElementById("botãoEnviar").  addEventListener("click",validaFormulario)
+
+function validaFomulario(){
+
+if(document.getElementById("nome").value !="" && document.getElementById("email").value !=""){  Prontinho! você receberá as novidades por email. 
+  
+}else{
+alert("Por favor, preencha os campos nome e email!")
+ }
+}
+  
+
